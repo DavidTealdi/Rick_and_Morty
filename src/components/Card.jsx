3bc220745b98ece81b9c8b06254d8f15121caf4b
David Tealdi <tealdidavid12@gmail.com>
2023-04-04 11:10:52 -0300

    'Etapa_2_Estado-lifeCycle'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card({onClose, name, status, species, gender, origin, image}) {
+export default function Card({onClose, name, status, species, gender, origin, image, id}) {
    return (
       <div>
-         <button onClick={onClose}>X</button>
+         <button onClick={() => onClose(id)}>X</button>
          <h2>Name: {name}</h2>
          <h2>Status: {status}</h2>
          <h2>Species: {species}</h2>
